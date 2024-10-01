cd374d6ba61d952e48cdb3537296e7aa88d177e7
Lurushin <luke.rushing@allsaintshigh.org>
2024-10-01 11:29:48 -0500

    Initial commit

--- src/Dog.java
+++ src/Dog.java
@@ -1,5 +1,41 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Dog {
+    private String name;
+    private int age;
+    private int multiplier;
+
+    public Dog(String n, int a, int m) {
+        name = n;
+        age = a;
+        multiplier = m;
     }
+    public Dog(String n, int a) {
+        name = n;
+        age = a;
+        multiplier = 7;
+    }
+    public Dog(String n) {
+        age = 1;
+        name = n;
+        multiplier = 7;
+    }
+    public Dog() {
+        age = 1;
+        name = "dog";
+        multiplier = 7;
+    }
+    public int getAge() {
+        return age;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getPeopleAge(int multiplier) {
+        return age * multiplier;
+    }
+    public void increaseAgeByOne() {
+        age ++;
+    }
+    /*public int increaseAgeByOne() {
+        return age ++;
+    }*/
 }
--- src/DogRunner.java
+++ src/DogRunner.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
-public class Dog {
+public class DogRunner {
+    public static void main(String[] args) {
+        Dog d = new Dog( "rover" );
+            System.out.println( d.getPeopleAge( 7 ) );
+            d.increaseAgeByOne();
+            d.increaseAgeByOne();
+            System.out.println( d.getPeopleAge( 11 ) );
+            System.out.println( d.getName() );
+            d.increaseAgeByOne();
+            d.increaseAgeByOne();
+            d.increaseAgeByOne();
+            d.increaseAgeByOne();
+            System.out.println( d.getPeopleAge( 8 ) );
+        Dog g = new Dog( "bob", 3 );
+            System.out.println( g.getName() );
+            System.out.println( g.getPeopleAge( 5 ) );
+    }
 }
